4085c1d43818c5c7016e9c3440fc4d12c55acc4e
MaxKits <macros5@ya.ru>
2014-12-17 16:32:24 +0400

    propeties added

--- src/JetAccess/Properties/AssemblyInfo.cs
+++ src/JetAccess/Properties/AssemblyInfo.cs
@@ -7,19 +7,12 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [ assembly : AssemblyTitle( "JetAccess" ) ]
-[ assembly : AssemblyDescription( "" ) ]
 [ assembly : AssemblyConfiguration( "" ) ]
-[ assembly : AssemblyCompany( "" ) ]
-[ assembly : AssemblyProduct( "JetAccess" ) ]
-[ assembly : AssemblyCopyright( "Copyright ©  2014" ) ]
-[ assembly : AssemblyTrademark( "" ) ]
-[ assembly : AssemblyCulture( "" ) ]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 
-[ assembly : ComVisible( false ) ]
 [ assembly : InternalsVisibleTo( "JetAccessTests" ) ]
 [ assembly : InternalsVisibleTo( "JetAccessTestsIntegration" ) ]
 
@@ -37,6 +30,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
-[ assembly : AssemblyVersion( "1.0.0.0" ) ]
-[ assembly : AssemblyFileVersion( "1.0.0.0" ) ]
